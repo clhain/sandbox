b7812cbc6f8634f7eacd4cc76c8966fc86d992f0
Chris Hain <c.hain@f5.com>
2022-09-29 15:34:53 -0700

    bump provider

--- bundle/terraform/main.tf
+++ bundle/terraform/main.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.43.0"
+      version = "~> 3.53.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.43.0"
+      version = "~> 3.53.0"
     }
   }
 }
