bce7f399799316ed6b8b97416eb27979020899b9
Chris Hain <c.hain@f5.com>
2022-09-29 15:38:04 -0700

    add back missing required var

--- bundle/terraform/main.tf
+++ bundle/terraform/main.tf
@@ -224,6 +224,7 @@ module "cloud-nat" {
   source                             = "terraform-google-modules/cloud-nat/google"
   version                            = "~> 2.0.0"
   region                             = var.region
+  project_id                         = var.project
   router                             = google_compute_router.router.name
   name                               =  "${var.cluster_name}-ingress-static-${random_string.suffix.result}"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
